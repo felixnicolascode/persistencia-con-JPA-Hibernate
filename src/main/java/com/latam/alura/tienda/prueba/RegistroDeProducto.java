89331c5ac32e3631115cb425d8fa558a53223833
Felix <felixnicolas-m@hotmail.com>
2023-09-14 21:00:58 -0300

    Registro de producto - Persistiendo una entidad #1

---
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -0,0 +1,28 @@
+package com.latam.alura.tienda.prueba;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.latam.alura.tienda.modelo.Producto;
+
+public class RegistroDeProducto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Producto celular = new Producto();
+		celular.setNombre("Samsung");
+		celular.setDescripcion("Telefono usado");
+		celular.setPrecio(new BigDecimal("1000"));
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tienda");
+		EntityManager em = factory.createEntityManager();
+		
+		em.persist(celular);
+
+	}
+
+}
